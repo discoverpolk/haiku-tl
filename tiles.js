023cb86f3343ffadc47faee5e42dab80192c6a13
Discover Polk CBD <49324668+discoverpolk@users.noreply.github.com>
2019-08-20 21:35:05 -0700

    basics

---
+++ tiles.js
@@ -0,0 +1,28 @@
+/* $(function(){
+	$('.front').click(function(){
+		$(this).toggleClass('flip');
+	});	
+    
+	$('.front').hover(function(){
+		$(this).toggleClass('flip');
+	});
+    
+    	$('.back').click(function(){
+		$(this).toggleClass('flip');
+	});	
+    
+	$('.back').hover(function(){
+		$(this).toggleClass('flip');
+	});
+	var objLen = $('.container .panel').length;
+	setInterval( function() {		
+		var randObj = getRandom(objLen);
+		var obj = $('.container .panel:eq('+randObj+')');
+		obj.toggleClass('flip');		
+	}, 1500); 
+});
+
+function getRandom(len){
+  return Math.floor(Math.random() * len);
+}
+*/
